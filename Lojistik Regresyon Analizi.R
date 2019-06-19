9818ed2f08d5cef819042f13725572cf5c113e8f
Mustafa Nayansak <32681985+MustafaNayansak@users.noreply.github.com>
2019-06-19 17:35:09 +0300

    Rename Lojistik Regresyon.R to Lojistik Regresyon Analizi.R

--- Lojistik Regresyon Analizi.R
+++ Lojistik Regresyon Analizi.R
@@ -3,7 +3,7 @@
 setwd("C:/Users/Asus/Desktop/Sixth Episode")
 
 
-# Veri Okuma İşlemi
+# Veri Okuma Ä°ÅŸlemi
 veri <- read.table(file = "uis.dat",header = F)
 str(veri)
 veri <- veri[,-1]
@@ -14,75 +14,75 @@ str(veri)
 attach(veri)
 
 
-# Modeli Kurma İşlemi
+# Modeli Kurma Ä°ÅŸlemi
 library(caret)
 model<-glm(DFREE~AGE+BECK+factor(IVHX)+NDRUGTX+
              RACE+TREAT+SITE,family = binomial)
 summary(model)
 
 
-# Modelin Anlamlılığını İnceleme
+# Modelin AnlamlÄ±lÄ±ÄŸÄ±nÄ± Ä°nceleme
 k1<-(model$df.null-model$df.residual) 
 table<-qchisq(0.95,k1) 
 g.full<-model$null.deviance-model$deviance 
 if(g.full>table) 
 {
-  print("H0 Red, %95 güvenle model anlamlıdır.") 
+  print("H0 Red, %95 gÃ¼venle model anlamlÄ±dÄ±r.") 
 }else{
-  print("H0 Reddedilemez, model %95 güvenle anlamlı değildir.")
+  print("H0 Reddedilemez, model %95 gÃ¼venle anlamlÄ± deÄŸildir.")
 }
 
 
-# Değişkenlerin Anlamlılıklarını İnceleme(Yaş Değişkeni)
+# DeÄŸiÅŸkenlerin AnlamlÄ±lÄ±klarÄ±nÄ± Ä°nceleme(YaÅŸ DeÄŸiÅŸkeni)
 b<-0.0504143
 st<-0.0174057
 wald<-b/st
 if(wald>abs(qnorm(0.025)))
 {
-  print("H0 hipotezi reddedilir. Yaş değişkeni, tedavi programı sonunda hastanın durumu üzerinde anlamlı bir katkısı vardır.")
+  print("H0 hipotezi reddedilir. YaÅŸ deÄŸiÅŸkeni, tedavi programÄ± sonunda hastanÄ±n durumu Ã¼zerinde anlamlÄ± bir katkÄ±sÄ± vardÄ±r.")
 }else{
-  print("H0 Reddedilemez, seçilen değişken %95 güvenle anlamlı değildir.")
+  print("H0 Reddedilemez, seÃ§ilen deÄŸiÅŸken %95 gÃ¼venle anlamlÄ± deÄŸildir.")
 }
 
 
-# Yeni Modeli Kurma(Anlamsız Değişkenleri Çıkarma)
+# Yeni Modeli Kurma(AnlamsÄ±z DeÄŸiÅŸkenleri Ã‡Ä±karma)
 model1<-glm(DFREE~AGE+factor(IVHX)+NDRUGTX+
               +TREAT,family = binomial)
 summary(model1)
 
 
-# Yeni Modelin Anlamlılığını Test Etme
+# Yeni Modelin AnlamlÄ±lÄ±ÄŸÄ±nÄ± Test Etme
 k2<-(model1$df.null-model1$df.residual) 
 table<-qchisq(0.95,k2) 
 g.azalt<-model1$null.deviance-model1$deviance 
 if(g.azalt<table) 
 {
-  print("H0 Red, %95 güvenle model anlamlıdır.") 
+  print("H0 Red, %95 gÃ¼venle model anlamlÄ±dÄ±r.") 
 }else{
-  print("H0 Reddedilemez, model %95 güvenle anlamlı değildir.")
+  print("H0 Reddedilemez, model %95 gÃ¼venle anlamlÄ± deÄŸildir.")
 }
 
 
-# Hangi Modelin Anlamlı Olduğunu Belirleme
+# Hangi Modelin AnlamlÄ± OlduÄŸunu Belirleme
 g3<-model1$deviance-model$deviance
 k.new<-k1-k2
 table1<-qchisq(0.95,k.new) 
 if(g3>k.new) 
 {
-  print("H0 Red, En az bir değişken sıfırdan farklıdır ve full model kullanılmalıdır.") 
+  print("H0 Red, En az bir deÄŸiÅŸken sÄ±fÄ±rdan farklÄ±dÄ±r ve full model kullanÄ±lmalÄ±dÄ±r.") 
 } else{
-  print("H0 Reddedilemez, modelden çıkarılan değişkenler bu model için anlamsızdır")
+  print("H0 Reddedilemez, modelden Ã§Ä±karÄ±lan deÄŸiÅŸkenler bu model iÃ§in anlamsÄ±zdÄ±r")
 }
 
 
-# Belirli Özelliklere Sahip Bir Bireyin Sonuç Değişkeni Tahmini
+# Belirli Ã–zelliklere Sahip Bir Bireyin SonuÃ§ DeÄŸiÅŸkeni Tahmini
 gx<-(-2.3376+33*0.05258-1*0.62366-3*0.06376+1*0.45134)
 gx
 p<-exp(gx)/(1+exp(gx))
 p
 
 
-# Sensitivity ve Specificity Değerleri
+# Sensitivity ve Specificity DeÄŸerleri
 y_fitnew<-c(veri)
 y_fit<-model$fitted.values
 cut<-0.5
@@ -106,7 +106,7 @@ specificity<-ctable[1,1]/(ctable[1,1]+ctable[1,2])
 sensitivity
 specificity
 
-# Modelin Tahmin Performansını Ölçmek için ROC eğrisi
+# Modelin Tahmin PerformansÄ±nÄ± Ã–lÃ§mek iÃ§in ROC eÄŸrisi
 library(ROSE)
 
 nrow(veri)
@@ -115,7 +115,7 @@ table(veri$DFREE)
 basari <- veri[veri$DFREE==1,]
 basarisizlik <- veri[veri$DFREE==0,]
 
-x <- nrow(basari) / nrow(basarisizlik)  #Başarı ile Başarısızlık satırlarının arasındaki kat oranı
+x <- nrow(basari) / nrow(basarisizlik)  #BaÅŸarÄ± ile BaÅŸarÄ±sÄ±zlÄ±k satÄ±rlarÄ±nÄ±n arasÄ±ndaki kat oranÄ±
 
 deneme.veri.evet <- basari[sample(nrow(basari),73),]  
 deneme.veri.hayir <- basarisizlik[sample(nrow(basarisizlik),73/x),]  
@@ -127,7 +127,7 @@ tahmin.veri.hayir <- basarisizlik[sample(nrow(basarisizlik), 73/x),]
 tahmin.veri <- rbind(tahmin.veri.evet, tahmin.veri.hayir)
 nrow(tahmin.veri)  
 
-# Model kurmak için oluşturduğumuz veri setini eğitiyoruz
+# Model kurmak iÃ§in oluÅŸturduÄŸumuz veri setini eÄŸitiyoruz
 model3 <- glm(model.veri$DFREE ~
                 model.veri$AGE +
                 model.veri$IVHX +
